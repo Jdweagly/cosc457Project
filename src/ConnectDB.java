311c926a7c9d7957aa7b6efb6268cedc48a8e5a9
Haleluya Getachew <41033709+hgetac1@users.noreply.github.com>
2020-04-26 12:14:17 -0400

    connect java with MySQL workbench

---
+++ src/ConnectDB.java
@@ -0,0 +1,44 @@
+import java.sql.Connection; 
+import java.sql.DriverManager;
+import java.sql.ResultSet; 
+import java.sql.SQLException;
+import java.sql.Statement; 
+
+
+public class ConnectDB {
+    
+    public static void main(String[] args) {
+        try{
+            Class.forName("com.mysql.jdbc.Driver"); 
+          
+        } catch (ClassNotFoundException e) {
+            System.out.println(e);
+        }
+        
+        final String ID = "emclau5"; 
+        final String PW = "Cosc*r7e8"; 
+        final String SERVER = "jdbc:mysql://triton.towson.edu:3360/"+ID+"db"; 
+       
+        
+        try{
+            Connection con = DriverManager.getConnection(SERVER,ID,PW); 
+            Statement stmt = con.createStatement(); 
+            
+            ResultSet rs = stmt.executeQuery("Select * from Customers");
+            
+            while (rs.next()) {
+                String Fname = rs.getString("FName"); 
+                String Lname = rs.getString("LName"); 
+                System.out.println(Fname + "," + Lname);
+            }
+            
+        }catch(SQLException e) {
+            System.err.println(e);
+        }
+        
+        
+    }
+     
+        
+    
+}
